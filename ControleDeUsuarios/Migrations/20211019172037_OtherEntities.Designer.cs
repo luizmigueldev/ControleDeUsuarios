dcc1085a9a35490d94c369092ca3c674f6ac041d
luizmigueldev <luizmigueln.dev@gmail.com>
2021-10-19 14:25:00 -0300

    Other entities and second migration

--- ControleDeUsuarios/Migrations/20211019172037_OtherEntities.Designer.cs
+++ ControleDeUsuarios/Migrations/20211019172037_OtherEntities.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using ControleDeUsuarios.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ControleDeUsuarios.Migrations
 {
     [DbContext(typeof(ControleDeUsuariosContext))]
-    partial class ControleDeUsuariosContextModelSnapshot : ModelSnapshot
+    [Migration("20211019172037_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
